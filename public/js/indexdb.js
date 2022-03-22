f207ef6a33db45abb13f793fc905a5943851d600
moonryc <70920813+moonryc@users.noreply.github.com>
2022-03-22 15:56:26 -0500

    changed and updated indexdb.js

--- public/js/indexdb.js
+++ public/js/indexdb.js
@@ -1,13 +1,13 @@
 // create variable to hold db connection
 let db;
-// establish a connection to IndexedDB database called 'pizza_hunt' and set it to version 1
+// establish a connection to IndexedDB database called 'budget_follower' and set it to version 1
 const request = indexedDB.open('budget_follower', 1);
 
 // this event will emit if the database version changes (nonexistant to version 1, v1 to v2, etc.)
 request.onupgradeneeded = (event)=> {
     // save a reference to the database
     const db = event.target.result;
-    // create an object store (table) called `new_pizza`, set it to have an auto incrementing primary key of sorts
+    // create an object store (table) called `new_transaction`, set it to have an auto incrementing primary key of sorts
     db.createObjectStore('new_transaction', {autoIncrement: true});
 };
 
@@ -16,7 +16,7 @@ request.onsuccess =  async (event) =>{
     // when db is successfully created with its object store (from onupgradedneeded event above) or simply established a connection, save reference to db in global variable
     db = event.target.result;
 
-    // check if app is online, if yes run uploadPizza() function to send all local db data to api
+    // check if app is online, if yes run uploadTransaction() function to send all local db data to api
     if (navigator.onLine) {
         await uploadTransactions()
     }
@@ -27,12 +27,12 @@ request.onerror = (event)=> {
     console.log(event.target.errorCode);
 };
 
-// This function will be executed if we attempt to submit a new pizza and there's no internet connection
+// This function will be executed if we attempt to submit a new transaction and there's no internet connection
 function saveRecord(record) {
     // open a new transaction with the database with read and write permissions
     const transaction = db.transaction(['new_transaction'], 'readwrite');
 
-    // access the object store for `new_pizza`
+    // access the object store for `new_transaction`
     const transactionObjectStore = transaction.objectStore('new_transaction');
 
     // add record to your store with add method
@@ -68,10 +68,10 @@ const uploadTransactions=async()=> {
                     }
                     // open one more transaction
                     const transaction = db.transaction(['new_transaction'], 'readwrite');
-                    // access the new_pizza object store
-                    const pizzaObjectStore = transaction.objectStore('new_pizza');
+                    // access the new_transaction object store
+                    const transactionObjectStore = transaction.objectStore('new_transaction');
                     // clear all items in your store
-                    pizzaObjectStore.clear();
+                    transactionObjectStore.clear();
 
                     alert('All transactions has been submitted!');
                 })
